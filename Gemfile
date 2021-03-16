51e5a7ab9cb133cf7ca8d9908e2d8be11db5f799
Vasyl Shevchenko <vasya.shevchenko.2011@gmail.com>
2021-03-16 09:27:30 +0200

    init magic_8_ball

---
+++ Gemfile
@@ -0,0 +1,6 @@
+source 'https://rubygems.org'
+
+gem 'byebug'
+gem 'rubocop', '~> 1.11', require: false
+gem 'rspec', '~> 3.10'
+gem 'telegram-bot', '~> 0.15.3'
---
+++ magic_8_ball.rb
@@ -0,0 +1,19 @@
+answers = [
+  "As I see it, yes (Мне кажется — «да»)",
+  "Most likely (Вероятнее всего)",
+  "Outlook good (Хорошие перспективы)",
+  "Signs point to yes (Знаки говорят — «да»)",
+  "Yes (Да)",
+  "Reply hazy, try again (Пока не ясно, попробуй снова)",
+  "Ask again later (Спроси позже)",
+  "Better not tell you now (Лучше не рассказывать)",
+  "Cannot predict now (Сейчас нельзя предсказать)",
+  "Concentrate and ask again (Сконцентрируйся и спроси опять)",
+  "Don’t count on it (Даже не думай)",
+  "My reply is no (Мой ответ — «нет»)",
+  "My sources say no (По моим данным — «нет»)",
+  "Outlook not so good (Перспективы не очень хорошие)",
+  "Very doubtful (Весьма сомнительно)",
+]
+
+p answers.sample
